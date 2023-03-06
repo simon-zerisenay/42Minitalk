13036658c8e51a053c8d44243d3d6281700634af
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-03-06 18:45:50 +0400

    Update client.c

--- client.c
+++ client.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
  
 #include <signal.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <unistd.h>   
 #include "libft/libft.h"  
  
